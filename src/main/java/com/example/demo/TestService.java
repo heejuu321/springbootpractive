35a5b5750203a465a3920f68f68757d48b09db9b
heejuu <heejuu321@gmail.com>
2023-02-20 18:16:03 +0900

    first commit

---
+++ src/main/java/com/example/demo/TestService.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class TestService {
+
+
+
+}
